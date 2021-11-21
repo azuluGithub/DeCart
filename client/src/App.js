1407fa1c5d904afbf8834fadf55b2c524cb06fe9
zuluarnold0 <66118771+zuluarnold0@users.noreply.github.com>
2021-11-21 20:23:43 +0200

    app component routes added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,32 @@
-import './App.css';
+//npm packages
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
+//local imports
+import './App.css';
+import NavBar from './components/nav_bar/NavBar';
+import Footer from './components/footer/Footer';
+import DashBoard from './pages/dash_board/DashBoard';
+import Cart from './pages/cart/Cart';
+import SignIn from './pages/auth/sign_in/SignIn';
+import SignUp from './pages/auth/sign_up/SignUp';
+
+/**
+ * # redirects to desired page
+ * # only one page is rendered
+ */
+const App = () => {
   return (
-    <div className="app">
-      App
-    </div>
-  );
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route exact path='/' component={DashBoard} />
+        <Route path='/cart' component={Cart} />
+        <Route path='/auth/signup' component={SignUp} />
+        <Route path='/auth/signin' component={SignIn} />
+      </Switch>
+      <Footer />
+    </Router>
+  )
 }
 
 export default App;
